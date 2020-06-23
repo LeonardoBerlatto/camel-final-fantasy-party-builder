b68a7085cc3d03567985acf68c853ab8126cc675
Leonardo <leoberlatto@gmail.com>
2020-06-23 16:26:01 -0300

    ✨Adding party builder service preset

---
+++ party-builder-service/src/test/java/br/com/finalfantasy/partybuilder/partybuilderservice/MemberJoiningPartyRequestControllerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.finalfantasy.partybuilder.partybuilderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberJoiningPartyRequestControllerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
