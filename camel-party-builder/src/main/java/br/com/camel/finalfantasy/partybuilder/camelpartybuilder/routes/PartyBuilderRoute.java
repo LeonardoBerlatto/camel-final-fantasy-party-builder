ff1f458e5eb7a697be9d2d95752ced6ad5c063a1
Leonardo <leoberlatto@gmail.com>
2020-06-23 14:24:45 -0300

    ✨Adding route to request data from moogleAPI

--- camel-party-builder/src/main/java/br/com/camel/finalfantasy/partybuilder/camelpartybuilder/routes/PartyBuilderRoute.java
+++ camel-party-builder/src/main/java/br/com/camel/finalfantasy/partybuilder/camelpartybuilder/routes/PartyBuilderRoute.java
@@ -1,6 +1,8 @@
 package br.com.camel.finalfantasy.partybuilder.camelpartybuilder.routes;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http.HttpMethods;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +11,20 @@ public class PartyBuilderRoute extends RouteBuilder {
     @Override
     public void configure() {
         from("file:input?noop=true&include=party_.*\\.xml").
-                to("file:saida");
+                routeId("main-route").
+                log("${body}").
+                setProperty("userId", xpath("/party/userId/text()")).
+                split().xpath("/party/members/name").
+                to("direct:ff-api-route").
+                to("direct:party-builder-service-route");
+
+        from("direct:ff-api-route").
+                routeId("ff-api-route").
+                log("${body}").
+                setProperty("name", xpath("/name/text()")).
+                log("Querying data from character: ${exchangeProperty.name}").
+                setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
+                setHeader(Exchange.HTTP_QUERY, simple("name=${exchangeProperty.name}")).
+                to("https://www.moogleapi.com/api/v1/characters/search");
     }
 }
