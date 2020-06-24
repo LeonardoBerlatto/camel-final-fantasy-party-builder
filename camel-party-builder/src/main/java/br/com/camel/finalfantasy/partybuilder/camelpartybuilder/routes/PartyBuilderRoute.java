c7279f450b039b9ba07d3968026ea204e001d970
Leonardo <leoberlatto@gmail.com>
2020-06-23 23:07:00 -0300

    ✨Adding docs

--- camel-party-builder/src/main/java/br/com/camel/finalfantasy/partybuilder/camelpartybuilder/routes/PartyBuilderRoute.java
+++ camel-party-builder/src/main/java/br/com/camel/finalfantasy/partybuilder/camelpartybuilder/routes/PartyBuilderRoute.java
@@ -8,15 +8,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class PartyBuilderRoute extends RouteBuilder {
 
+    public static final String BRACkETS_REGEX = "[\\[\\]']+";
+
     @Override
     public void configure() {
+        // read all party_**.xml files from input folder
         from("file:input?noop=true&include=party_.*\\.xml").
                 routeId("main-route").
                 log("${body}").
-                split().xpath("/parties/party").
+                split().xpath("/parties/party"). // splits for every party
                 log("${body}").
                 setProperty("partyId", xpath("/party/partyId/text()")).
-                split().xpath("/party/members/name").
+                split().xpath("/party/members/name"). // splits for every party member to add to the party
                 to("direct:ff-api-route").
                 to("direct:party-builder-service-route");
 
@@ -27,7 +30,7 @@ public class PartyBuilderRoute extends RouteBuilder {
                 log("Querying data from character: ${exchangeProperty.name}").
                 setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
                 setHeader(Exchange.HTTP_QUERY, simple("name=${exchangeProperty.name}")).
-                to("https://www.moogleapi.com/api/v1/characters/search");
+                to("https://www.moogleapi.com/api/v1/characters/search"); // sends a http request to moogleAPI to get party member info
 
         from("direct:party-builder-service-route").
                 routeId("party-builder-service-route").
@@ -35,12 +38,12 @@ public class PartyBuilderRoute extends RouteBuilder {
                 process(exchange -> {
                     String body = exchange.getIn().getBody(String.class);
 
-                    exchange.getIn().setBody(body.replaceAll("[\\[\\]']+",""));
+                    exchange.getIn().setBody(body.replaceAll(BRACkETS_REGEX, "")); // removes square brackets from body
                 }).
                 log("${body}").
                 setHeader(Exchange.HTTP_METHOD, HttpMethods.POST).
                 setHeader(Exchange.HTTP_PATH, simple("${exchangeProperty.partyId}")).
                 removeHeader(Exchange.HTTP_QUERY).
-                to("http://localhost:8081/api/members/party");
+                to("http://localhost:8081/api/members/party"); // sends a http request to local party builder API to add party member
     }
 }
