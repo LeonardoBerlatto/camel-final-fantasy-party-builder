fc13a1c2a10112974c074d9df5e5fc25d9d76e33
Leonardo <leoberlatto@gmail.com>
2020-07-05 15:19:58 -0300

    🚑 Fixing container broken endpoints

--- camel-party-builder/src/main/java/br/com/camel/finalfantasy/partybuilder/camelpartybuilder/repository/impl/PartyBuilderRepositoryImpl.java
+++ camel-party-builder/src/main/java/br/com/camel/finalfantasy/partybuilder/camelpartybuilder/repository/impl/PartyBuilderRepositoryImpl.java
@@ -4,6 +4,7 @@ import br.com.camel.finalfantasy.partybuilder.camelpartybuilder.repository.Party
 import br.com.camel.finalfantasy.partybuilder.camelpartybuilder.representation.FinalFantasyCharacter;
 import br.com.camel.finalfantasy.partybuilder.camelpartybuilder.utils.HttpEntityUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,11 +14,12 @@ public class PartyBuilderRepositoryImpl implements PartyBuilderRepository {
 
     public final RestTemplate restTemplate;
 
-    public static final String PARTY_BUILDER_SERVICE_URI = "http://localhost:8081/api/members/party";
+    @Value("${party-builder.api.url}")
+    private String partyBuilderServiceUri;
 
     @Override
     public void addCharacterToParty(FinalFantasyCharacter character, long partyId) {
-        final var requestURI = PARTY_BUILDER_SERVICE_URI+"/"+partyId;
+        final var requestURI = partyBuilderServiceUri+"/"+partyId;
 
         restTemplate.postForEntity(requestURI, HttpEntityUtils.createJSONRequestEntity(character), FinalFantasyCharacter.class);
     }
--- camel-party-builder/src/test/java/br/com/camel/finalfantasy/partybuilder/camelpartybuilder/repository/impl/PartyBuilderRepositoryImplTest.java
+++ camel-party-builder/src/test/java/br/com/camel/finalfantasy/partybuilder/camelpartybuilder/repository/impl/PartyBuilderRepositoryImplTest.java
@@ -3,18 +3,20 @@ package br.com.camel.finalfantasy.partybuilder.camelpartybuilder.repository.impl
 import br.com.camel.finalfantasy.partybuilder.camelpartybuilder.fixture.FinalFantasyCharacterFixture;
 import br.com.camel.finalfantasy.partybuilder.camelpartybuilder.representation.FinalFantasyCharacter;
 import br.com.camel.finalfantasy.partybuilder.camelpartybuilder.utils.HttpEntityUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
-import static br.com.camel.finalfantasy.partybuilder.camelpartybuilder.repository.impl.PartyBuilderRepositoryImpl.PARTY_BUILDER_SERVICE_URI;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -22,15 +24,24 @@ import static org.mockito.Mockito.when;
 @DisplayName("Party Builder Repository")
 class PartyBuilderRepositoryImplTest {
 
+    private final String PARTY_BUILDER_API_URI = "http://test";
+
     @Mock
     private RestTemplate restTemplate;
 
     @InjectMocks
     private PartyBuilderRepositoryImpl tested;
 
+    @BeforeEach
+    void setUp() {
+        ReflectionTestUtils
+                .setField(tested, "partyBuilderServiceUri", PARTY_BUILDER_API_URI);
+    }
+
     @Test
-    @DisplayName("Should call the API to add characters to the part")
+    @DisplayName("Should call the API to add characters to the party")
     void testAddCharacterToParty() {
+        // arrange
         final var character = FinalFantasyCharacterFixture.get()
                 .withRandomData()
                 .build();
@@ -39,10 +50,10 @@ class PartyBuilderRepositoryImplTest {
 
         final long partyId = Long.parseLong("23");
 
-        // arrange
+        final HttpEntity httpEntity = HttpEntityUtils.createJSONRequestEntity(character);
         when(restTemplate.postForEntity(
-                PARTY_BUILDER_SERVICE_URI + "/" + partyId,
-                HttpEntityUtils.createJSONRequestEntity(character),
+                PARTY_BUILDER_API_URI+"/"+partyId,
+                httpEntity,
                 FinalFantasyCharacter.class)
         ).thenReturn(response);
 
@@ -51,8 +62,8 @@ class PartyBuilderRepositoryImplTest {
 
         // assert
         verify(restTemplate).postForEntity(
-                PARTY_BUILDER_SERVICE_URI + "/" + partyId,
-                HttpEntityUtils.createJSONRequestEntity(character),
+                PARTY_BUILDER_API_URI+"/"+partyId,
+                httpEntity,
                 FinalFantasyCharacter.class);
     }
 }
